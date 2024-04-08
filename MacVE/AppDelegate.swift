50b45f80b285254e434c7efc0ee568395db111fc
lugalu <luizjaf01@gmail.com>
2024-04-08 20:51:16 -0300

    Project Setup

---
+++ MacVE/AppDelegate.swift
@@ -0,0 +1,25 @@
+//Created by Lugalu on 08/04/24.
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
