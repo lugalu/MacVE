558bd87b9a9803ef177cf7bf387edb4580d63760
lugalu <luizjaf01@gmail.com>
2024-04-08 21:39:08 -0300

    Feat: A wild Window has Appeared

--- MacVE/AppDelegate.swift
+++ MacVE/AppDelegate.swift
@@ -15,6 +15,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         mainMenu.addItem(menu)
         
         NSApplication.shared.mainMenu = mainMenu
+        
+        let minSize = NSSize(width: 1200, height: 700)
+        
+        let window = NSWindow(
+            contentRect: NSRect(
+                x: 0,
+                y: 0,
+                width: 1200,
+                height: 700
+            ),
+            styleMask: [
+                .titled,
+                .closable,
+                .fullSizeContentView,
+                .resizable
+            ],
+            backing: .buffered,
+            defer: false
+        )
+        window.minSize = minSize
+        //
+                
+        if let size = NSScreen.main?.frame.size {
+            window.maxSize = size
+           // window.setFrameTopLeftPoint(NSPoint(x: abs(size.width - minSize.width / 4), y: abs(size.height - minSize.height)))
+            window.setFrameOrigin( NSPoint(x: abs(size.width - minSize.width) / 2, y: abs(size.height - minSize.height) / 2 ))
+
+        }
+        
+        window.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
