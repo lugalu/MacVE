bdcae681c0126310737ee7d95b43b67aea4a1086
lugalu <luizjaf01@gmail.com>
2024-06-11 17:23:45 -0300

    SwiftUI project

---
+++ MacVE/ContentView.swift
@@ -0,0 +1,19 @@
+//Created by Lugalu on 11/06/24.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacVE/MacVEApp.swift
@@ -0,0 +1,13 @@
+//Created by Lugalu on 11/06/24.
+
+import SwiftUI
+
+@main
+struct MacVEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 1200, minHeight: 700)
+        }
+    }
+}
