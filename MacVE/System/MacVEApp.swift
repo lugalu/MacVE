dc74f567fa8459dd3b2f24b55d62df2d1c36d54e
lugalu <luizjaf01@gmail.com>
2024-07-16 15:15:23 -0300

    removed a bunch of stuff, saving to disk in any capacity is not available right now, I will study the possibility again in the future.

--- MacVE/System/MacVEApp.swift
+++ MacVE/System/MacVEApp.swift
@@ -4,19 +4,13 @@ import SwiftUI
 
 @main
 struct MacVEApp: App {
+    
     var body: some Scene {
-        Window("Project Select", id: "SelectorView") {
-            ProjectSelectorView<ProjectSelectorModel>()
-                .environment(ProjectSelectorModel())
-        }
-        .windowResizability(.contentSize)
-
         WindowGroup("Project", id: "PlaybackView" ){
             PlaybackView<PlaybackModel>()
                 .environmentObject(PlaybackModel())
         }
         .defaultSize(width: 1200, height: 700)
         .windowResizability(.contentSize)
-
     }
 }
